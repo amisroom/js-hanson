fe28dc03da9518972aeace3c2f29cc8bf1f997c2
Ami Murase <ami.murase.jpn@gmail.com>
2021-03-14 22:02:10 +0900

    Work11 fetch APIを使ってデータを取得する

--- main.js
+++ main.js
@@ -2,7 +2,6 @@
 
 const ul = document.getElementById("list");
 const div = document.getElementById("loading");
-<<<<<<< HEAD
 //  // 3秒後にresolveされた結果を表示する関数定義
 // const resolveAfter3Seconds= () => {
 //   // Promiseオブジェクトを返す
@@ -26,36 +25,10 @@ const div = document.getElementById("loading");
 //     }, 3000);
 //   });
 // }
-=======
- // 3秒後にresolveされた結果を表示する関数定義
-const resolveAfter3Seconds= () => {
-  // Promiseオブジェクトを返す
-  return new Promise((resolve) => {
-    // タイマーをセットする
-    setTimeout(() => {
-      resolve([
-        {
-          to: "bookmark.html",
-          img: "img/1.png",
-          alt: "画像1",
-          text: "ブックマーク"
-        },
-        {
-          to: "message.html",
-          img: "img/2.png",
-          alt: "画像2",
-          text: "メッセージ"
-        }
-      ]);
-    }, 3000);
-  });
-}
->>>>>>> b64d84346753dba329ce1541613072b6391a0a90
 
 // 非同期関数を定義
 async function asyncCall() {
 try {
-<<<<<<< HEAD
   const response = await fetch('https://jsondata.okiba.me/v1/json/FtQ63210306142524')
   const json = await response.json();
   const data = await json.data;
@@ -67,15 +40,6 @@ try {
     createElements();
     loading.style.display = "none";
   }, 3000);
-=======
-  // 3秒後にresolveされた結果を表示する関数定義を定数valuesに格納
-  const values = await resolveAfter3Seconds();
-  createElements(values);
-} catch(error) {
-  console.log('error');
-} finally {
-  loading.style.display = "none";
->>>>>>> b64d84346753dba329ce1541613072b6391a0a90
 }
 }
     asyncCall();
@@ -96,8 +60,4 @@ try {
         li.appendChild(a);
         a.appendChild(img);
       }
-<<<<<<< HEAD
   }
-=======
-  }
->>>>>>> b64d84346753dba329ce1541613072b6391a0a90
