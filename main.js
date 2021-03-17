70ebf2454f313650e2928ad70731aa373068b8fb
Ami Murase <ami.murase.jpn@gmail.com>
2021-03-17 20:15:26 +0900

    SetTimeout関数とtry-catch-finallyの記述修正

--- main.js
+++ main.js
@@ -2,47 +2,24 @@
 
 const ul = document.getElementById("list");
 const div = document.getElementById("loading");
-//  // 3秒後にresolveされた結果を表示する関数定義
-// const resolveAfter3Seconds= () => {
-//   // Promiseオブジェクトを返す
-//   return new Promise((resolve) => {
-//     // タイマーをセットする
-//     setTimeout(() => {
-//       resolve([
-//         {
-//           to: "bookmark.html",
-//           img: "img/1.png",
-//           alt: "画像1",
-//           text: "ブックマーク"
-//         },
-//         {
-//           to: "message.html",
-//           img: "img/2.png",
-//           alt: "画像2",
-//           text: "メッセージ"
-//         }
-//       ]);
-//     }, 3000);
-//   });
-// }
 
 // 非同期関数を定義
 async function asyncCall() {
 try {
+  // JSONを読み込む
   const response = await fetch('https://jsondata.okiba.me/v1/json/FtQ63210306142524')
   const json = await response.json();
-  const data = await json.data;
-  return data;
+  createElements(json.data);
 } catch(error) {
   console.log('error');
 } finally {
-  setTimeout(() => {
-    createElements(items);
-    loading.style.display = "none";
-  }, 3000);
+  loading.style.display = "none";
 }
 }
-    asyncCall();
+
+setTimeout(() => {
+  asyncCall();
+}, 3000);
 
 // DOMを作る関数
     function createElements(items) {
