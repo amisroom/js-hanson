8028af8d8bbe280034d6cb55b6edd19aec8194e6
Ami Murase <ami.murase.jpn@gmail.com>
2021-03-19 19:25:41 +0900

    setTimeout関数の位置訂正

--- main.js
+++ main.js
@@ -20,35 +20,32 @@ try {
 }
 }
 
-setTimeout(() => {
-  asyncCall();
-}, 3000);
 
 // DOMを作る関数
-    function createElements(items) {
-      for (const e of items) {
-        const a = document.createElement('a');
-        const img = document.createElement('img');
-        const li = document.createElement('li');
-  
-        a.href = e.to;
-        img.src = e.img;
-        img.alt = e.alt;
-        a.textContent = e.text;
-  
-        ul.appendChild(li);
-        li.appendChild(a);
-        a.appendChild(img);
-      }
+function createElements(items) {
+  for (const e of items) {
+    const a = document.createElement('a');
+    const img = document.createElement('img');
+    const li = document.createElement('li');
+    
+    a.href = e.to;
+    img.src = e.img;
+    img.alt = e.alt;
+    a.textContent = e.text;
+    
+    ul.appendChild(li);
+    li.appendChild(a);
+    a.appendChild(img);
   }
+}
 
-  // ボタンをクリックした時にローディング画像を表示しボタンを消す関数
-  function clickButton() {
-    loading.style.display = 'inline';
-    btn.style.display = 'none';
-  }
+// ボタンをクリックした時にローディング画像を表示しボタンを消す関数
+function clickButton() {
+  loading.style.display = 'inline';
+  btn.style.display = 'none';
+}
 
-  // ボタンをクリックした時にイベントを発生させる
-  btn.addEventListener('click', () => {
-    clickButton();
-  });
+// ボタンをクリックした時にイベントを発生させる
+btn.addEventListener('click', () => {
+  clickButton();
+});
